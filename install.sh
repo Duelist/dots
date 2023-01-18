730eb894da81b04d9b3ad18a9ea1e2d53ef4de53
Ian Benedict <Duelist@users.noreply.github.com>
2023-01-18 09:14:12 -0500

    :sparkles: [Installer] Update install script.

--- install.sh
+++ install.sh
@@ -1,8 +1,4 @@
-# Link ~/.vimrc and other files to ~/dots/.vimrc
-mkdir -p ~/.config/nvim
+# Symlink configuration
 ln -s ~/dots/.zshrc ~/.zshrc
-ln -s ~/dots/.vimrc ~/.config/nvim/init.vim
-ln -s ~/dots/.tmux.conf ~/.tmux.conf
-ln -s ~/dots/.tmuxinator ~/.tmuxinator
-
-# Install and set up libraries such as nerd fonts, base16-shell, etc.
+ln -s ~/dots/.config/nvim ~/.config/nvim
+ln -s ~/dots/.config/kitty ~/.config/kitty
